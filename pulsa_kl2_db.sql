34dbb1061d46b16c39e3741ea23f843734c4ec62
naufal56-xtg <naufalseries56@gmail.com>
2023-06-01 17:49:58 +0700

    Insert pulsa_kl2_db.sql

--- pulsa_kl2_db.sql
+++ pulsa_kl2_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2023 at 07:18 AM
+-- Generation Time: Jun 01, 2023 at 12:48 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -30,11 +30,28 @@ SET time_zone = "+00:00";
 CREATE TABLE `histories` (
   `id` int(11) NOT NULL,
   `no_kartu` varchar(20) NOT NULL,
-  `provider` varchar(25) NOT NULL,
+  `prov` varchar(25) NOT NULL,
   `nominal` int(11) NOT NULL,
   `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `histories`
+--
+
+INSERT INTO `histories` (`id`, `no_kartu`, `prov`, `nominal`, `tanggal`, `user_id`) VALUES
+(1, '08123193131', 'tri', 10000, '2023-06-01 04:04:39', 1),
+(2, '08999123131', 'im3', 10000, '2023-06-01 04:17:04', 1),
+(3, '08100001', 'im3', 10000, '2023-06-01 04:39:18', 1),
+(4, '08100002', 'xl', 10000, '2023-06-01 04:39:25', 1),
+(5, '08100003', 'xl', 10000, '2023-06-01 04:39:55', 1),
+(6, '08100004', 'xl', 10000, '2023-06-01 04:40:37', 1),
+(7, '0810012313', 'xl', 10000, '2023-06-01 04:40:45', 1),
+(8, '098213131', 'tsel', 10000, '2023-06-01 04:40:54', 1),
+(9, '088123131', 'tri', 10000, '2023-06-01 04:47:28', 1),
+(10, '081313123', 'xl', 10000, '2023-06-01 04:47:34', 1),
+(11, '0898813131', 'xl', 10000, '2023-06-01 04:47:45', 1);
 
 -- --------------------------------------------------------
 
@@ -53,8 +70,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `name`, `saldo`) VALUES
-(1, 'Naufal', 120000),
-(2, 'Taufik', 20000),
+(1, 'Naufal', 110000),
+(2, 'Taufik', 120000),
 (3, 'Apriyanto', 0);
 
 --
@@ -82,13 +99,23 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `histories`
 --
 ALTER TABLE `histories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `histories`
+--
+ALTER TABLE `histories`
+  ADD CONSTRAINT `histories_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
